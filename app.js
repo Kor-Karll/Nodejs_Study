4548d6ba0cbec20136f515e3fbd859e9eff4abf3
Kor-Karll <hankikab@gmail.com>
2019-07-24 16:12:22 +0900

    express 연결 / 라우팅

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('First Commit');
+// console.log('First Commit');
+
+const express = require('express');
+const app = express();
+app.get('/', (req,res)=>{
+    res.send('Hello home page');
+});
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Login please</h1>');
+});
+
+app.listen(3000,()=>{
+    console.log('Hello world');
+});
