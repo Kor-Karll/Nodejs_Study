5569c5ac22d4771ff2f9c77b7423b6661c5692f5
Kor-Karll <hankikab@gmail.com>
2019-07-25 17:54:41 +0900

    express 웹페이지 표시

--- app.js
+++ app.js
@@ -8,6 +8,33 @@ app.get('/', (req,res)=>{
     res.send('Hello home page');
 });
 
+app.get('/dynamic', (req,res)=>{
+    var lis = '';
+    for(var i =0 ; i < 5 ; i++)
+    {
+        lis = lis + '<li>coding</li>';
+    }
+
+    var time = Date();
+    var output = `<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>Page Title</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+        <script src='main.js'></script>
+    </head>
+    <body>
+        Hello, Dynamic!
+        <ul>${lis}</ul>
+        ${time}
+    </body>
+    </html>`;
+    res.send(output);
+});
+
 app.get('/route', (req,res)=>{
     res.send('Hello Router, <img src="/route.png">');
 });
