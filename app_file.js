a7e7d13135146e4159633d8fe64351b1323afd27
Kor-Karll <hankikab@gmail.com>
2019-07-29 14:29:35 +0900

    코드 개선

--- app_file.js
+++ app_file.js
@@ -11,38 +11,42 @@ app.set('views','./views_file');
 app.set('view engine','jade');
 
 app.get('/topic/new',(req,res)=>{
-    res.render('new');
-});
-
-app.get('/topic/:id',(req,res)=>{
-    var id = req.params.id;
     fs.readdir('data',(err,files)=>{
         if(err)
         {
             console.log(err);
             res.status(500).send('Internal Server Error');
         }
-        fs.readFile('data/'+id,'utf-8',(err,data)=>{
-            if(err)
-            {
-                res.status(500).send('Internal Server Error');
-            }
-            res.render('view',{'title':id,'description':data,'topics':files});
-        });
+        res.render('new',{'topics':files});
     });
 });
 
-app.get('/topic',(req,res)=>{
-    fs.readdir('data',(err,files)=>{
-        if(err)
-        {
-            console.log(err);
-            res.status(500).send('Internal Server Error');
-        }
-
-        res.render('view',{'topics':files});
-    });
+app.get(['/topic','/topic/:id'],(req,res)=>{
     
+    fs.readdir('data',(err,files)=>{
+        if(err)
+        {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+
+        var id = req.params.id;
+        if(id)
+        {
+            fs.readFile('data/'+id,'utf-8',(err,data)=>{
+                if(err)
+                {
+                    res.status(500).send('Internal Server Error');
+                }
+                res.render('view',{'title':id,'description':data,'topics':files});
+            });
+        }
+        else
+        {
+            res.render('view',{'title':'Welcome','description':'Hello, JavaScript','topics':files});
+        }
+        
+    });
 });
 
 app.post('/topic',(req,res)=>{
@@ -55,7 +59,7 @@ app.post('/topic',(req,res)=>{
             res.status(500).send('Internal Server Error')
         }
 
-        res.send('Success!');
+        res.redirect('/topic/'+title);
     });
 });
 
